202488d1272d68afdec71536f21bdc97ef4c1da6
GOPINATHAN <get4gopim@gmail.com>
2020-03-10 15:28:29 +0530

    Refactor Moving Package

--- src/com/pod/Main.java
+++ src/com/pod/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test com.example.learning.Main");
     }
+
+    private void test() {
+        System.out.println("");
+    }
 }
