dfbdf7d0bac2cb6d57899e072f1db787c9878349
GOPINATHAN <get4gopim@gmail.com>
2020-03-10 16:49:49 +0530

    Remove un used file

--- src/com/pod/Main.java
+++ src/com/pod/Main.java
@@ -1,11 +1,28 @@
 package com.pod;
 
 public class Main {
+
+    private String userName;
+
+    public Main(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public static void main(String[] args) {
         System.out.println("Test com.example.learning.Main");
     }
 
     private void test() {
         System.out.println("");
+        Main c2 = new Main("3");
+        Main c1 = new Main("23");
     }
 }
